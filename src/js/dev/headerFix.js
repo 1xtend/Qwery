ab9dedafe0cc28f03c26aa145938408a5d1c6cf5
1xtend <vlad.mezhevyi@gmail.com>
2023-05-23 19:30:12 +0300

    information section

--- src/js/dev/headerFix.js
+++ src/js/dev/headerFix.js
@@ -2,12 +2,13 @@ import { mainVars } from '../app.js';
 
 export const headerFix = () => {
   const header = document.querySelector('.header');
-  let headerHeight = header.clientHeight;
+  // let headerHeight = header.clientHeight;
+  let srollHeight = 50;
 
   window.addEventListener('scroll', () => {
-    if (window.scrollY > headerHeight) {
+    if (window.scrollY > srollHeight) {
       header.classList.add('header-fix');
-    } else if (window.scrollY <= headerHeight && !mainVars.body.classList.contains('body-lock')) {
+    } else if (window.scrollY <= srollHeight && !mainVars.body.classList.contains('body-lock')) {
       header.classList.remove('header-fix');
     }
   });
